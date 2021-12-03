4b9e92addbc4e817aa75e6644f9ad97ad25375e9
ipeinado <i.peinado.martinez@gmail.com>
2021-12-03 12:43:24 +0100

    added ckeditor markdown

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -24,6 +24,7 @@ include __DIR__ . "/settings.pantheon.php";
  * https://www.drupal.org/project/drupal/issues/3091285
  */
 // $settings['skip_permissions_hardening'] = TRUE;
+$config['config_split.config_split.local_development']['status'] = FALSE;
 
 /**
  * If there is a local settings file, then include it
